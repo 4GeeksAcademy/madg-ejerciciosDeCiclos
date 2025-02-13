7b9392964074053860776781f7edd61b27e2ebd8
Madg26 <manux_852@hotmail.com>
2025-02-13 00:00:38 +0000

    [Se uso "for of" ] Se optimizo la funcion para que se vea mas compacta.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
+const pronouns = ['the', 'our'];
+const adjs = ['great', 'big'];
+const nouns = ['jogger', 'racoon'];
 
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
-      console.log(pronoun[i] + adj[j] + noun[k] + ".com" + ".es");
-    };
+// for (let i = 0; i < pronouns.length; i++) {
+//   for (let j = 0; j < adjs.length; j++) {
+//     for (let k = 0; k < nouns.length; k++) {
+//       console.log(pronouns[i] + adjs[j] + nouns[k] + ".com" + ".es");
+//     };
+//   }
+// }
+for (const pronoun of pronouns){
+  for (const adj of adjs){
+    for (const noun of nouns){
+      console.log(pronoun + adj + noun + ".com");
+      
+    }
   }
+    
+  
 }
